0af2da2479fbb1e8adeb1a7047b60a2020c74045
Owen1Campbell <ocampbell@knights.ucf.edu>
2022-03-16 17:44:03 -0400

    Tier 1

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,8 +8,16 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d;
 
     public float speed;
+    public float jumpForce = 3f;
 
     public Text score;
+    public GameObject winMsg;
+
+    // variables for ground checking are included, but unused to enable wall jumping
+    public Transform groundCheck;
+    private bool isGrounded;
+    public float checkRadius;
+    public LayerMask allGround;
 
     private int scoreValue = 0;
 
@@ -18,25 +26,33 @@ public class PlayerScript : MonoBehaviour
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winMsg.SetActive(false);
+    }
+    
+    private void Update()
+    {
+        if (scoreValue == 4)
+        {
+            winMsg.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, allGround);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) // using OnTrigger instead of OnCollision prevents coins from stopping the player's movement
     {
-       if (collision.collider.tag == "Coin")
+        if (collision.tag == "Coin")
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+            Destroy(collision.gameObject);
         }
-
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -45,7 +61,7 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
+                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
             }
         }
     }
